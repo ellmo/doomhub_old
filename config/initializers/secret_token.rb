77561a34f1f343291b7a9a11f772f3fd7b9465d7
ellmo <ellmunzai@gmail.com>
2011-12-06 17:49:46 +0100

    INIT COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doomhub::Application.config.secret_token = '4df6d32e25640eeb8c0d7ebb9690341b890e48069e40ea2f2d02ead22e1e6ec18ff6cb56a9bc489d6be3e91fd80521d0b4ef65e8ebb9ca71bd39b94a7c8d6c1e'
